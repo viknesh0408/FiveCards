89e4ca7f949d98d7879e4b4c8c6121bdb189ee2d
Viknesh <viknesh0408@gmail.com>
2026-06-13 23:33:35 +0530

    backend not connect to frontend issue solved

--- backend/src/main/java/com/game/tickgame/controller/VersionController.java
+++ backend/src/main/java/com/game/tickgame/controller/VersionController.java
@@ -14,8 +14,8 @@ public class VersionController {
     @GetMapping
     public Map<String,String> getVersion() {
         return Map.of(
-            "version","1.0.14",
-            "apkUrl","https://drive.google.com/uc?export=download&id=1Pthd_OZmY0j79x1ffFT2p54GKWGoeeQo"
+            "version","1.0.15",
+            "apkUrl","https://drive.google.com/uc?export=download&id=12JmMbKTMrC9WZIzCnQy-ANQyzCUY3rWT"
         );
     }
 }
